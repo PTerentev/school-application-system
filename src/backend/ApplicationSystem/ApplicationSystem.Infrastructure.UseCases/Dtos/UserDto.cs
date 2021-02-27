097c3f4d20e55644f09e984d0e50e1ac8ebe05ba
Pavel Terentev <pavel.terentyev@saritasa.com>
2021-02-27 23:21:24 +0700

    Add some admin commands

---
+++ src/backend/ApplicationSystem/ApplicationSystem.Infrastructure.UseCases/Dtos/UserDto.cs
@@ -0,0 +1,38 @@
+﻿namespace ApplicationSystem.Infrastructure.UseCases.Dtos
+{
+    /// <summary>
+    /// User DTO.
+    /// </summary>
+    public class UserDto
+    {
+        /// <summary>
+        /// Id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// First name.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Last name.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Middle name.
+        /// </summary>
+        public string MiddleName { get; set; }
+
+        /// <summary>
+        /// User's post.
+        /// </summary>
+        public string Post { get; set; }
+
+        /// <summary>
+        /// Email.
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
