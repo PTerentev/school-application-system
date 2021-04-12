92b17f343417cd03408added44a817d333dc556b
Pavel Terentev <pavel.terentyev@saritasa.com>
2021-04-12 18:14:39 +0700

    Add commands to manage authorities

---
+++ src/backend/ApplicationSystem/ApplicationSystem.Infrastructure.UseCases/Admin/CreateAuthority/CreateAuthorityCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using ApplicationSystem.DataAccess;
+
+namespace ApplicationSystem.Infrastructure.UseCases.Admin.CreateAuthority
+{
+    /// <summary>
+    /// Create authority command handler.
+    /// </summary>
+    internal class CreateAuthorityCommandHandler : IRequestHandler<CreateAuthorityCommand>
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+        private readonly IMapper mapper;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="applicationDbContext">Application database context.</param>
+        /// <param name="mapper">Mapper.</param>
+        public CreateAuthorityCommandHandler(
+            ApplicationDbContext applicationDbContext, 
+            IMapper mapper)
+        {
+            this.mapper = mapper;
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        /// <inheritdoc/>
+        public async Task<Unit> Handle(CreateAuthorityCommand request, CancellationToken cancellationToken)
+        {
+            var authority = mapper.Map<Domain.Entities.Authority>(request);
+            applicationDbContext.Authorities.Add(authority);
+
+            await applicationDbContext.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/backend/ApplicationSystem/ApplicationSystem.Infrastructure.UseCases/Admin/RemoveAuthority/RemoveAuthorityCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace ApplicationSystem.Infrastructure.UseCases.Admin.RemoveAuthority
+{
+    /// <summary>
+    /// Remove authority command.
+    /// </summary>
+    public class RemoveAuthorityCommand : IRequest
+    {
+        /// <summary>
+        /// Authority Id.
+        /// </summary>
+        public int AuthorityId { get; set; }
+    }
+}
